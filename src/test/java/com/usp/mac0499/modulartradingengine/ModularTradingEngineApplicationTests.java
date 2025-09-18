8b1091d146f079d181fed2a3d442f27c325d7be6
Gabriel Arrais <arrais.gbr@usp.br>
2025-09-17 21:23:48 -0300

    chore(Project): initial project structure

---
+++ src/test/java/com/usp/mac0499/modulartradingengine/ModularTradingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usp.mac0499.modulartradingengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModularTradingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
