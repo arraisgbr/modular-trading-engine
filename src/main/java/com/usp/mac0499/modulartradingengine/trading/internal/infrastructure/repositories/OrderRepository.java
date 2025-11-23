688c9540bf7f60014c46cece046c64f8db816329
Gabriel Arrais <arrais.gbr@usp.br>
2025-11-23 16:19:36 -0300

    feat(Trading): add calls to external methods

--- src/main/java/com/usp/mac0499/modulartradingengine/trading/internal/infrastructure/repositories/OrderRepository.java
+++ src/main/java/com/usp/mac0499/modulartradingengine/trading/internal/infrastructure/repositories/OrderRepository.java
@@ -1,5 +1,7 @@
 package com.usp.mac0499.modulartradingengine.trading.internal.infrastructure.repositories;
 
+import com.usp.mac0499.modulartradingengine.sharedkernel.domain.values.Money;
+import com.usp.mac0499.modulartradingengine.sharedkernel.domain.values.OrderType;
 import com.usp.mac0499.modulartradingengine.trading.internal.domain.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -22,5 +24,8 @@ public interface OrderRepository extends JpaRepository<Order, UUID> {
 
     @Query("SELECT o FROM Order o WHERE o.portfolioId = :assetId AND o.status <> 'CANCELED'")
     List<Order> findValidOrdersByPortfolioId(@Param("portfolioId") UUID portfolioId);
+
+    @Query("SELECT o FROM Order o WHERE o.assetId = :assetId AND o.price = :value AND o.type = :orderType AND o.status <> 'CANCELED'")
+    Optional<Order> findOrderByAssetIdAndValueAndType(UUID assetId, Money value, OrderType orderType);
 
 }
--- src/main/java/com/usp/mac0499/modulartradingengine/trading/internal/service/OrderServiceInternal.java
+++ src/main/java/com/usp/mac0499/modulartradingengine/trading/internal/service/OrderServiceInternal.java
@@ -1,5 +1,9 @@
 package com.usp.mac0499.modulartradingengine.trading.internal.service;
 
+import com.usp.mac0499.modulartradingengine.catalog.external.IAssetServiceExternal;
+import com.usp.mac0499.modulartradingengine.portfolio.external.IPortfolioServiceExternal;
+import com.usp.mac0499.modulartradingengine.sharedkernel.domain.values.Money;
+import com.usp.mac0499.modulartradingengine.sharedkernel.domain.values.OrderType;
 import com.usp.mac0499.modulartradingengine.trading.internal.domain.entities.Order;
 import com.usp.mac0499.modulartradingengine.trading.internal.domain.exceptions.OrderNotFoundException;
 import com.usp.mac0499.modulartradingengine.trading.internal.infrastructure.repositories.OrderRepository;
@@ -7,6 +11,7 @@ import com.usp.mac0499.modulartradingengine.trading.internal.service.interfaces.
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,10 +20,17 @@ import java.util.UUID;
 public class OrderServiceInternal implements IOrderServiceInternal {
 
     private final OrderRepository orderRepository;
+    private final IPortfolioServiceExternal portfolioService;
+    private final IAssetServiceExternal assetService;
 
     @Override
     public Order createOrder(Order order) {
-        return orderRepository.save(order);
+        if (order.getType() == OrderType.BUY) {
+            portfolioService.reserveBalance(order.getPortfolioId(), order.getPrice().multiply(new Money(BigDecimal.valueOf(order.getQuantity()))));
+        } else {
+            portfolioService.reserveAsset(order.getPortfolioId(), order.getAssetId(), order.getQuantity());
+        }
+        return tryToMatchOrder(order);
     }
 
     @Override
@@ -35,10 +47,27 @@ public class OrderServiceInternal implements IOrderServiceInternal {
     public void cancelOrder(UUID id) {
         orderRepository.findById(id).ifPresentOrElse(order -> {
             order.cancelOrder();
+            if (order.getType() == OrderType.BUY) {
+                portfolioService.releaseBalance(order.getPortfolioId(), order.getPrice().multiply(new Money(BigDecimal.valueOf(order.getQuantity()))));
+            } else {
+                portfolioService.releaseAsset(order.getPortfolioId(), order.getAssetId(), order.getQuantity());
+            }
             orderRepository.save(order);
         }, () -> {
             throw new OrderNotFoundException(id);
         });
     }
 
+    private Order tryToMatchOrder(Order order) {
+        orderRepository.findOrderByAssetIdAndValueAndType(order.getAssetId(), order.getPrice(), order.getType() == OrderType.BUY ? OrderType.SELL : OrderType.BUY).ifPresent(matchOrder -> {
+            var sellOrder = order.getType() == OrderType.SELL ? order : matchOrder;
+            var buyOrder = order.getType() == OrderType.BUY ? order : matchOrder;
+            matchOrder.cancelOrder();
+            order.cancelOrder();
+            orderRepository.save(matchOrder);
+            portfolioService.executeTransaction(buyOrder.getPortfolioId(), sellOrder.getPortfolioId(), order.getAssetId(), order.getQuantity(), order.getPrice());
+            assetService.updateAsset(order.getAssetId(), order.getPrice());
+        });
+        return orderRepository.save(order);
+    }
 }
