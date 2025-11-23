360ec5586fa74906dccff8182bfe77ed8564d8a1
Gabriel Arrais <arrais.gbr@usp.br>
2025-11-23 16:41:45 -0300

    fix(Trading): query for orders created by a portfolioId

--- src/main/java/com/usp/mac0499/modulartradingengine/trading/internal/infrastructure/repositories/OrderRepository.java
+++ src/main/java/com/usp/mac0499/modulartradingengine/trading/internal/infrastructure/repositories/OrderRepository.java
@@ -22,7 +22,7 @@ public interface OrderRepository extends JpaRepository<Order, UUID> {
     @Query("SELECT o FROM Order o WHERE o.assetId = :assetId AND o.status <> 'CANCELED'")
     List<Order> findValidOrdersByAssetId(@Param("assetId") UUID assetId);
 
-    @Query("SELECT o FROM Order o WHERE o.portfolioId = :assetId AND o.status <> 'CANCELED'")
+    @Query("SELECT o FROM Order o WHERE o.portfolioId = :portfolioId AND o.status <> 'CANCELED'")
     List<Order> findValidOrdersByPortfolioId(@Param("portfolioId") UUID portfolioId);
 
     @Query("SELECT o FROM Order o WHERE o.assetId = :assetId AND o.price = :value AND o.type = :orderType AND o.status <> 'CANCELED'")
